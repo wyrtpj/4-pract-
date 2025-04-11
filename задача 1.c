627ed5d570eb92ada15825d5086713f5ca2bdbe8
маш маш маш <mtnkuv@mail.ru>
2025-04-11 08:43:18 +0300

    Create задача 1.c

---
+++ задача 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    
+    int red_balls[10];
+    
+      for(int i=0; i<10; i++)
+      {
+          red_balls[i]=i+1;
+      }
+    
+    printf("содержимое коробки: ");
+    
+        for (int i=0; i<10; i++)
+        {
+            printf("%d" , red_balls[i]);
+        }
+     
+    int size = sizeof(red_balls) / sizeof(red_balls[0]);
+    
+    printf ("В коробке помещается %d шариков.\n",size);
+    
+    return 0;
+}
